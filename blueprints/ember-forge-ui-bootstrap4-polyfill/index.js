ed3e5a7c7a19716be934e4ebeab03113a1b4bc85
Jeremy Brown <jeremy@notmessenger.com>
2016-07-28 13:32:43 -0500

    Add blueprint

---
+++ blueprints/ember-forge-ui-bootstrap4-polyfill/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    return this.addAddonsToProject({
+      packages: [
+        'ember-forge/ember-forge-ui-bootstrap4',
+      ]
+    });
+  }
+};
