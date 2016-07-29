648c338be8405272589b7305f082fe22472cb4f6
Jeremy Brown <jeremy@notmessenger.com>
2016-07-29 06:35:26 -0500

    Add ef-list-header component

---
+++ addon/components/ef-list-header.js
@@ -0,0 +1,55 @@
+import Component from 'ember-forge-ui/components/ef-list-header';
+import Ember from 'ember';
+
+const {
+  get
+} = Ember;
+
+/**
+ * @module
+ * @augments ember-forge-ui/components/ef-list-header
+ */
+export default Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  /**
+   * Manage class names dependent on list type and context of usage
+   *
+   * Ordered lists do not have any classes applied
+   * Unordered lists are considered list groups and have their own class applied
+   * Unordered lists used as navs have their own class applied
+   *
+   * @private
+   * @override
+   * @returns {undefined}
+   */
+  setContextClasses() {
+    this._super(...arguments);
+
+    if (!get(this, 'ordered')) {
+      get(this, 'classNames').addObject((get(this, 'usedAs') === 'nav') ? 'nav-item' : 'list-group-item');
+    }
+  }
+
+});
